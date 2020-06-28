ba2495eb565ac7516de5b4c074c9c6ab4a9981aa
Mathieu Fontaine <mathieu.antoine.fontaine@gmail.com>
2020-06-28 19:23:21 +0200

    Updates

---
+++ precache-manifest.f4d4049db35efada5661a7e652e8e62b.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc0479387eca26498cc882e02f19c8d",
+    "url": "/react-weather-forecast-app/index.html"
+  },
+  {
+    "revision": "969f6c9bfe1676e97c3f",
+    "url": "/react-weather-forecast-app/static/css/2.db88180d.chunk.css"
+  },
+  {
+    "revision": "876559d5ab3bcecdb422",
+    "url": "/react-weather-forecast-app/static/css/main.6adc2d05.chunk.css"
+  },
+  {
+    "revision": "969f6c9bfe1676e97c3f",
+    "url": "/react-weather-forecast-app/static/js/2.93c4ad33.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-weather-forecast-app/static/js/2.93c4ad33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876559d5ab3bcecdb422",
+    "url": "/react-weather-forecast-app/static/js/main.043640e4.chunk.js"
+  },
+  {
+    "revision": "41efd139e3be5887f69e",
+    "url": "/react-weather-forecast-app/static/js/runtime-main.9d6610d1.js"
+  },
+  {
+    "revision": "2a513d516cb2d0dc4d7cdb5e74de9823",
+    "url": "/react-weather-forecast-app/static/media/blue.2a513d51.jpg"
+  },
+  {
+    "revision": "4f13aa96315d4e99c5ea1b4c9c6601d3",
+    "url": "/react-weather-forecast-app/static/media/clear.4f13aa96.jpg"
+  },
+  {
+    "revision": "bb5fe09d0dcce3270528edeb2bb7a8b2",
+    "url": "/react-weather-forecast-app/static/media/clear1.bb5fe09d.jpg"
+  },
+  {
+    "revision": "081b09e90d430428a2af260e6f3b89c3",
+    "url": "/react-weather-forecast-app/static/media/clear2.081b09e9.jpg"
+  },
+  {
+    "revision": "d747e1053858cf5824874d9af0aa74aa",
+    "url": "/react-weather-forecast-app/static/media/clear3.d747e105.jpg"
+  },
+  {
+    "revision": "4a6c3b95c023e9c342076e73223ed6d2",
+    "url": "/react-weather-forecast-app/static/media/clouds.4a6c3b95.jpg"
+  },
+  {
+    "revision": "02d6444edd7f21a7a55651dad66518f5",
+    "url": "/react-weather-forecast-app/static/media/clouds1.02d6444e.jpg"
+  },
+  {
+    "revision": "fce1ac56b987f58126a2cb3d2d2b609c",
+    "url": "/react-weather-forecast-app/static/media/clouds2.fce1ac56.jpg"
+  },
+  {
+    "revision": "f55d9cfe58d2f2d94589ddd8cd03805a",
+    "url": "/react-weather-forecast-app/static/media/clouds3.f55d9cfe.jpg"
+  },
+  {
+    "revision": "4dfa788552c1915d543b2ebeca1a971f",
+    "url": "/react-weather-forecast-app/static/media/clouds4.4dfa7885.jpg"
+  },
+  {
+    "revision": "5a45f1712877f133f87670bff19d2ffb",
+    "url": "/react-weather-forecast-app/static/media/clouds5.5a45f171.jpg"
+  },
+  {
+    "revision": "56af5f5d42fbdbd206b04ed4acb63db5",
+    "url": "/react-weather-forecast-app/static/media/fog.56af5f5d.jpg"
+  },
+  {
+    "revision": "757d6333629c41fd12c258a9dfe0c992",
+    "url": "/react-weather-forecast-app/static/media/rain.757d6333.jpg"
+  },
+  {
+    "revision": "ade49d8d4ca18f2fd7dc64dc0c9e72ae",
+    "url": "/react-weather-forecast-app/static/media/rain1.ade49d8d.jpg"
+  },
+  {
+    "revision": "4d5f535af687d101a1d50cef6f03d6ec",
+    "url": "/react-weather-forecast-app/static/media/rain10.4d5f535a.jpg"
+  },
+  {
+    "revision": "67d4d3d510323af3ad03fb6b881cf6fe",
+    "url": "/react-weather-forecast-app/static/media/rain2.67d4d3d5.jpg"
+  },
+  {
+    "revision": "983b8d88b005fa85ab86452ea32f01c0",
+    "url": "/react-weather-forecast-app/static/media/rain3.983b8d88.jpg"
+  },
+  {
+    "revision": "e1a996a91657d46388ebfdb83d9f3efe",
+    "url": "/react-weather-forecast-app/static/media/rain4.e1a996a9.jpg"
+  },
+  {
+    "revision": "4cfc03a0358f410f553cf28581476575",
+    "url": "/react-weather-forecast-app/static/media/rain5.4cfc03a0.jpg"
+  },
+  {
+    "revision": "0c62c419534fd9ce9dc7ff041fd30a1b",
+    "url": "/react-weather-forecast-app/static/media/rain6.0c62c419.jpg"
+  },
+  {
+    "revision": "316473c00341aaa2457f6e9a76d18f8e",
+    "url": "/react-weather-forecast-app/static/media/rain7.316473c0.jpg"
+  },
+  {
+    "revision": "2dd88391092b3dd05702c2f08fd6223a",
+    "url": "/react-weather-forecast-app/static/media/rain8.2dd88391.jpg"
+  },
+  {
+    "revision": "8bab8c668356851a0dfa0b77119f4825",
+    "url": "/react-weather-forecast-app/static/media/rain9.8bab8c66.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-weather-forecast-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-weather-forecast-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-weather-forecast-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-weather-forecast-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "de3540360962bfbf1ffc02c803bf4303",
+    "url": "/react-weather-forecast-app/static/media/snow.de354036.jpg"
+  },
+  {
+    "revision": "8190b6a05b2839045546c5fc1f3e4b0d",
+    "url": "/react-weather-forecast-app/static/media/thunderstorm.8190b6a0.jpg"
+  },
+  {
+    "revision": "b3d3a0dbb871cf5cb424c0c254e3c975",
+    "url": "/react-weather-forecast-app/static/media/thunderstorm1.b3d3a0db.jpg"
+  },
+  {
+    "revision": "9d97549da22b22e7f45fa847c8eed85a",
+    "url": "/react-weather-forecast-app/static/media/thunderstorm2.9d97549d.jpg"
+  }
+]);
